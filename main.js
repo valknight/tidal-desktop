3e1947f0d1f4510333a27e781f73a76a14edafe6
Vallerie Knight <vallerie.knight@gmail.com>
2019-02-19 01:01:30 +0000

    Cleanup build script, and auto move files such as flash

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var nativefier = require('nativefier').default;
+var fs = require('fs');
+var colors=require('colors');
 // we use nativefier, and just inject code, because that's all we need really
 
 var options = {
-    name: 'TIDAL', 
+    name: 'TIDAL',
     targetUrl: 'https://listen.tidal.com', // required
     out: 'build/',
     asar: false, // we don't need to package our code in an archive
@@ -29,8 +31,30 @@ var options = {
 
 nativefier(options, function(error, appPath) {
     if (error) {
-        console.error(error);
+        console.log(error.red);
         return;
     }
-    console.log('App has been nativefied to', appPath);
+    fs.copyFile('./libpepflashplayer.so', './'+appPath+'/libpepflashplayer.so', function(error) {
+        if(error) {
+            if(error['code']=='ENOENT'){
+                console.log("Could not copy libpepflashplayer.so : Is libpepflashplayer.so in the current directory?".bold.red)
+            }
+            else {
+                console.log(error.red);
+            }
+            return;
+        }
+        else {
+            // we now need to ensure that nativefier.json is set right (sometimes the name option doesn't pass correctly)
+            var rawdata = fs.readFileSync(appPath+'/resources/app/nativefier.json');  
+            var packageData = JSON.parse(rawdata);
+            packageData['name'] = "TIDAL";
+            // parts of our README ask people to change this file, so indenting it is polite
+            var to_write = JSON.stringify(packageData, null, 4); 
+            fs.writeFile(appPath+'/resources/app/nativefier.json', to_write, function(error) {
+                if(error) throw error;
+            })
+            console.log(("TIDAL is ready to start. cd to \""+appPath+"\", and run ./tidal").bold.green)
+        }
+    });
 });
